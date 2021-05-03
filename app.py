ae416de40ad598a902b199447598c51de9a5d95c
Perryvon <Perry.j.vr@gmail.com>
2021-05-03 12:25:07 -0700

    added second GET route

--- app.py
+++ app.py
@@ -2,6 +2,12 @@
 from flask import Flask, request, jsonify
 from models import db, connect_db, Cupcake
 
+# from flask_debugtoolbar import DebugToolbarExtension
+# toolbar = DebugToolbarExtension(app)
+
+# comment out if you want to see/pause redirects 
+# app.config[‘DEBUG_TB_INTERCEPT_REDIRECTS’] = False
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
@@ -18,3 +24,12 @@ def get_cupcake_list():
     serialized = [c.serialize() for c in cupcakes]
 
     return jsonify(cupcakes=serialized)
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake_data(cupcake_id):
+    """get data on a single cupcake"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+    
+    return jsonify(cupcake=serialized)
